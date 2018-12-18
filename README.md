47bfb47b162d8f8707a7fa6d7ecbd33c656c742d
Akash Shaw <shawakash54@gmail.com>
2018-12-18 11:28:15 +0530

    react setup, apollo client setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-#React app for GraphQL_basics
+# GraphQL React client
+
+We will be using Apollo with React to make GraphQL queries from the front-end. Apollo is GraphQL client.
+
+ApolloProvider is used to inject the data from Apollo into the application.
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {BookList} from './components'
+import ApolloClient from 'apollo-boost'
+import {URI} from '../lib/constants'
+import {ApolloProvider} from 'react-apollo'
+
+//Apollo Client setup
+const client = new ApolloClient({
+  uri: URI
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Reading List:</h1>
+          <BookList/>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
