2b40ba835819f033656024231ebe4f37078118bf
Akash Shaw <shawakash54@gmail.com>
2018-12-22 10:20:30 +0530

    deployment to heroku

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,3 @@
-// export const URI = "https://graphql-basics.herokuapp.com/graphql"
+export const URI = "https://graphql-basics.herokuapp.com/graphql"
 
-export const URI = "http://192.168.88.171:4000/graphql"
+// export const URI = "http://192.168.88.171:4000/graphql"
